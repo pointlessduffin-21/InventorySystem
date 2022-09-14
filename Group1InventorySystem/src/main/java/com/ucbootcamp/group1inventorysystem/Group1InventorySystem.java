f9cc997afab826289668e114821d06f2825dd5a2
pointlessduffin-21 <francisroelabarca14@gmail.com>
2022-09-14 17:12:51 +0800

    Import of the project file for Git Cloning

---
+++ Group1InventorySystem/src/main/java/com/ucbootcamp/group1inventorysystem/Group1InventorySystem.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.ucbootcamp.group1inventorysystem;
+
+/**
+ *
+ * @author yeems214
+ */
+public class Group1InventorySystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
